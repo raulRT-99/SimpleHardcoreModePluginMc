d52f8ab8b91b9f5df958ab5c8496142baf84271c
raulRT-99 <raul.reyestorres99@gmail.com>
2026-04-10 19:01:20 -0600

    starting project

---
+++ src/main/java/org/raul/plugins/simpleHardcoreMode/SimpleHardcoreMode.java
@@ -0,0 +1,16 @@
+package org.raul.plugins.simpleHardcoreMode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleHardcoreMode extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
